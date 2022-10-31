91e5a32201be209b3b025adcc7252f81d0951eba
sivag <sivag@SIVA-PC>
2022-10-31 20:13:38 +0530

    Second change

--- TryGIT/src/com/siva/GitTry.java
+++ TryGIT/src/com/siva/GitTry.java
@@ -5,6 +5,8 @@ public class GitTry {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GIT");
+		System.out.println("It is my first change");
+
 
 	}
 
